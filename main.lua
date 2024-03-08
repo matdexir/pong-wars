03a7f3c6c7296475f66bc3bdecc5bb72861eb45d
matdexir <matthieudesir21@gmail.com>
2024-03-08 12:32:14 +0800

    chore: added config file

--- main.lua
+++ main.lua
@@ -2,13 +2,6 @@ function love.load()
 	io.stdout:setvbuf("no")
 	Object = require("libs.classic")
 	require("game")
-	success = love.window.setMode(600, 600, {})
-	font = love.graphics.newFont(14)
-	love.graphics.setFont(font)
-	if not success then
-		print("UNABLE TO RESIZE")
-		love.event.quit(1)
-	end
 	game = Game()
 end
 
