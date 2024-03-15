c1006526983cd8035a0a4c69786b5572db706520
matdexir <matthieudesir21@gmail.com>
2024-03-15 20:59:51 +0800

    misc: added interesting font

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 function love.load()
 	io.stdout:setvbuf("no")
 	Object = require("libs.classic")
+	font = love.graphics.newFont("fonts/Open24DisplaySt.ttf", 18)
+	love.graphics.setFont(font)
 	require("game")
 	game = Game()
 end
