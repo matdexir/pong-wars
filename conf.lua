114f648ff5e77248a2e1fbe839dc283c2b56fc92
matdexir <matthieudesir21@gmail.com>
2024-03-15 20:49:42 +0800

    feat: added a rudimentary scoring system

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	-- success = love.window.setMode(600, 600, {})
 	t.window.width = 600
-	t.window.height = 600
+	t.window.height = 700
 	t.window.title = "Pong Wars Love2D"
 	t.window.resizable = false
 	t.version = "11.4"
